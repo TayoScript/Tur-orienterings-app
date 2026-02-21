d85adec4959caf54d238790937e060ff47a2234b
TayoScript <lawaltayo9@gmail.com>
2026-02-21 20:30:39 +0100

    token error

--- turorienteringapp/frontend/src/components/DeleteUser.js
+++ turorienteringapp/frontend/src/components/DeleteUser.js
@@ -15,11 +15,7 @@ const DeleteUser = () => {
   
   const handleDeleteAccount = async () => {
     try {
-<<<<<<< Updated upstream
       const token = localStorage.getItem('token');
-=======
-      const token = localStorage.getItem("token");
->>>>>>> Stashed changes
       const response = await fetch(`${API_BASE_URL}/api/v1/users/deleteMyProfile`, {
         method: 'DELETE',
         headers: {
